88cc8dd6fd385a724917e0650a16ec01fc5101f0
Nur Farzana <ciklanana@gmail.com>
2022-06-04 01:41:35 +0800

    Product listing

--- src/views/pages/Dashboard.js
+++ src/views/pages/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
-import { Navbar, Container } from 'react-bootstrap';
+import { Navbar, Container, Spinner } from 'react-bootstrap';
 import { Button, Row, Col, Card } from 'react-bootstrap';
+import { Link } from "react-router-dom";
 import Header from '../components/header/Header';
 import Home from './Home';
 import Login from './Login';
@@ -9,17 +10,66 @@ import '../../App.css';
 import logo from '../../logo.svg';
 
 class Dashboard extends Component {
+  // Constructor
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: [],
+      DataisLoaded: false
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://fakestoreapi.com/products?limit=12")
+    .then((res) => res.json())
+    .then((json) => {
+      this.setState({
+        products: json,
+        DataisLoaded: true
+      });
+    })
+  }
+
   render() {
+    const { DataisLoaded, products } = this.state;
+    if (!DataisLoaded) return <div>
+    <Row className="mx-0 justify-content-md-center">
+      <Col className="p-5 text-center">
+        <Spinner className="mx-auto" animation="border" variant="warning" />
+        <h3 className="text-center"> Fetching products.... </h3>
+      </Col>
+    </Row>
+    </div> ;
+
     return (
-      <Row className="mx-0 justify-content-md-center">
-        <Col md={8} className="p-5">
-        <Card className="px-5 py-3 bg-yellow">
-          <Card.Body>
-            <h2 className="text-center">Welcome to Dashboard! </h2>
-          </Card.Body>
-        </Card>
-        </Col>
-      </Row>
+      <React.Fragment>
+        <Row className="mx-0 justify-content-md-center">
+          <Col md={8} className="p-5">
+          <Card className="bg-yellow">
+            <Card.Body>
+              <h2 className="text-center">Welcome back, User! </h2>
+            </Card.Body>
+          </Card>
+          </Col>
+        </Row>
+        <Row className="mx-0 justify-content-md-center px-5 pb-5"> {
+          products.map((product) => (
+          <Col md={6} lg={3} className="p-3 d-flex align-items-stretch">
+          <Card className="p-3" style={{ width: '100%' }}>
+            <Card.Img className="product-img" variant="top" src={ product.image } />
+            <Card.Body className="d-flex flex-column">
+              <Card.Title><Link to={`/products/${product.id}`}>{ product.title }</Link></Card.Title>
+              <Card.Text>
+                <p>RM { product.price }</p>
+              </Card.Text>
+              <Button className="btn btn-lg btn-block btn-primary mt-auto" variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+          </Col>
+        ))
+}
+        </Row>
+      </React.Fragment>
     );
   }
 }
